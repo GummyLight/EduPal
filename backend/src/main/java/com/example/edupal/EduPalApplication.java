0e40e5365d7078ce46196158e37bdd966b360019
橡皮糖 <1323858543@qq.com>
2025-05-15 07:58:51 +0800

    Add backend directory

---
+++ backend/src/main/java/com/example/edupal/EduPalApplication.java
@@ -0,0 +1,13 @@
+package com.example.edupal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduPalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduPalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/edupal/EduPalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edupal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
