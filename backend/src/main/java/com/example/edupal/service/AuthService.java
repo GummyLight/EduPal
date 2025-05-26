96bf5fdbf0ac527a36998f3320bd1c92d15ae367
橡皮糖 <1323858543@qq.com>
2025-05-26 16:46:22 +0800

    feat:优化了response的格式，修改了登录的response接口，数据库增加了User类里面的user_name

--- backend/src/main/java/com/example/edupal/service/AuthService.java
+++ backend/src/main/java/com/example/edupal/service/AuthService.java
@@ -1,6 +1,5 @@
 package com.example.edupal.service;
 
-import com.example.edupal.model.User;
 import com.example.edupal.common.Result;
 public interface AuthService {
     Result registerUser(String userId, String password, String phoneNum, Integer userType);
