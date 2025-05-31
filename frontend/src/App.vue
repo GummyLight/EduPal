f55d8221564f99fcb4bad2477ed84ffbbb32f951
XunMeng <1851220295@qq.com>
2025-05-31 14:49:00 +0800

    blah blah blah

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+import { createApp } from 'vue';
+import router from './router';
+
+const app = createApp({
+  router,
+});
+
+app.mount('#app');
+</script>
