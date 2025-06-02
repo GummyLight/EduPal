6ed8139bf18ebd14b04b018740e58e44a9665541
橡皮糖 <1323858543@qq.com>
2025-06-02 10:39:28 +0800

    feat(admin):新增了管理员注册学生、教师、管理员功能

--- backend/src/main/java/com/example/edupal/dto/request/RegisterRequest.java
+++ backend/src/main/java/com/example/edupal/dto/request/RegisterRequest.java
@@ -4,15 +4,29 @@ import lombok.Data;
 
 @Data
 public class RegisterRequest {
+    private String userName;
     private String userId;
     private String password;
     private String email;
     private Integer userType;
+    private String class1;
+    private String class2;
+    private String teachingSubject;
+    private String studentClass;
+    private Integer studentGender;
 
-    public RegisterRequest(String userId, String password, String email, Integer userType) {
+    public RegisterRequest(String userName, String userId, String password, String email,
+                             Integer userType, String class1, String class2,
+                             String teachingSubject, String studentClass, Integer studentGender) {
+        this.userName = userName;
         this.userId = userId;
         this.password = password;
         this.email = email;
         this.userType = userType;
+        this.class1 = class1;
+        this.class2 = class2;
+        this.teachingSubject = teachingSubject;
+        this.studentClass = studentClass;
+        this.studentGender = studentGender;
     }
 }
