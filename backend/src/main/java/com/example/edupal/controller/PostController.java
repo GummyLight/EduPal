34847f47732bab22f48a31b911e5ee1272fc3bbc
chestnut <1208134539@qq.com>
2025-06-04 22:38:20 +0800

    不知道改了什么但是改了

--- backend/src/main/java/com/example/edupal/controller/PostController.java
+++ backend/src/main/java/com/example/edupal/controller/PostController.java
@@ -2,17 +2,13 @@ package com.example.edupal.controller;
 
 import com.example.edupal.dto.request.PostForm;
 import com.example.edupal.dto.request.ReplyForm;
-import com.example.edupal.dto.response.AttachedFileDTO;
 import com.example.edupal.dto.response.PostDTO;
 import com.example.edupal.dto.response.ReplyDTO;
 import com.example.edupal.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 import java.util.List;
