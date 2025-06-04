7f4b8daf98c0227775735fcb89089007d8726357
橡皮糖 <1323858543@qq.com>
2025-06-04 18:07:12 +0800

    feat(quiz):增加了

--- backend/src/main/java/com/example/edupal/model/PostCollection.java
+++ backend/src/main/java/com/example/edupal/model/PostCollection.java
@@ -1,30 +1,30 @@
-//package com.example.edupal.model;
-//
-//import jakarta.persistence.*;
-//import lombok.Data;
-//import java.io.Serializable;
-//import java.time.LocalDateTime;
-//
-//@Entity
-//@Table(name = "post_collection")
-//@Data
-//@IdClass(PostCollection.PostCollectionId.class)
-//public class PostCollection {
-//    @Id
-//    @Column(name = "user_Id", length = 255)
-//    private String userId;
-//
-//    @Id // 需要为复合主键的每个部分添加@Id注解
-//    @Column(name = "post_id") // 修正列名与数据库一致
-//    private String postId; // 需要添加postId字段，与PostCollectionId匹配
-//
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "post_Id", insertable = false, updatable = false)
-//    private Post post;
-//
-//    @Data
-//    public static class PostCollectionId implements Serializable {
-//        private String userId;
-//        private String postId;
-//    }
-//}
+package com.example.edupal.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "post_collection")
+@Data
+@IdClass(PostCollection.PostCollectionId.class)
+public class PostCollection {
+    @Id
+    @Column(name = "user_Id", length = 255)
+    private String userId;
+
+    @Id // 需要为复合主键的每个部分添加@Id注解
+    @Column(name = "post_id") // 修正列名与数据库一致
+    private String postId; // 需要添加postId字段，与PostCollectionId匹配
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_Id", insertable = false, updatable = false)
+    private Post post;
+
+    @Data
+    public static class PostCollectionId implements Serializable {
+        private String userId;
+        private String postId;
+    }
+}
