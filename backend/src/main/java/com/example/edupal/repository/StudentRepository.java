85fc6412f8d3384b250b96f17c32ae19b1006e96
橡皮糖 <1323858543@qq.com>
2025-06-05 08:44:25 +0800

    feat(repository):增加了student DAO层用来查询班级学生的数量和list的功能

--- backend/src/main/java/com/example/edupal/repository/StudentRepository.java
+++ backend/src/main/java/com/example/edupal/repository/StudentRepository.java
@@ -2,6 +2,7 @@ package com.example.edupal.repository;
 
 import com.example.edupal.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,10 @@ public interface StudentRepository extends JpaRepository<Student, String> {
     List<Student> findByStudentClass(String studentClass);
 
     List<Student> findByStudentName(String studentName);
+
+    @Query("SELECT COUNT(s) FROM Student s WHERE s.studentClass =: class1 OR s.studentClass =: class2")
+    Integer countByStudentClass(String class1, String class2);
+
+    @Query("SELECT s FROM Student s WHERE s.studentClass =: class1 OR s.studentClass =: class2")
+    List<Student> findByStudentClass(String class1, String class2);
 }
