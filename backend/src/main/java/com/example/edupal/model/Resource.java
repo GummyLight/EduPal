d58a013bacc6c8a0384d61316f2e03e39ba9e29d
橡皮糖 <1323858543@qq.com>
2025-06-05 22:54:09 +0800

    style(Resource):对于Resource的实体类变量做了驼峰命名法的统一

--- backend/src/main/java/com/example/edupal/model/Resource.java
+++ backend/src/main/java/com/example/edupal/model/Resource.java
@@ -1,97 +1,39 @@
 package com.example.edupal.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Setter
+@Getter
 @Entity
 public class Resource {
+    // Getters and Setters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 可选，表示自动增长
-    private int resource_id;
+    private int resourceId;
 
 
     @Column(length = 8, nullable = false)
     private String subject;
 
     @Column(length = 10)
-    private String teacher_id;
+    private String teacherId;
 
     @Lob
-    private String resource_content;
+    private String resourceContent;
 
     @Column(length = 4)
-    private String class_id;
+    private String classId;
 
     @Column(length = 50)
     private String name;
 
-    private LocalDateTime upload_time;
+    private LocalDateTime uploadTime;
 
     @Column(length = 255)
     private String description;
 
-    // Getters and Setters
-    public int getResource_id() {
-        return resource_id;
-    }
-
-    public void setResource_id(int resource_id) {
-        this.resource_id = resource_id;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getTeacher_id() {
-        return teacher_id;
-    }
-
-    public void setTeacher_id(String teacher_id) {
-        this.teacher_id = teacher_id;
-    }
-
-    public String getResource_content() {
-        return resource_content;
-    }
-
-    public void setResource_content(String resource_content) {
-        this.resource_content = resource_content;
-    }
-
-    public String getClass_id() {
-        return class_id;
-    }
-
-    public void setClass_id(String class_id) {
-        this.class_id = class_id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getUpload_time() {
-        return upload_time;
-    }
-
-    public void setUpload_time(LocalDateTime upload_time) {
-        this.upload_time = upload_time;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
